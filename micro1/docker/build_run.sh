09f17930ad8da3e1d5c6a373c47e348e00b086c1
Igor Murashoff <igor.murashoff@gmail.com>
2016-12-08 12:30:50 +0200

    remove static content

--- micro1/docker/build_run.sh
+++ micro1/docker/build_run.sh
@@ -1,6 +1,4 @@
-sudo /usr/local/bin/docker-compose build
-#sudo /usr/local/bin/docker-compose run db1 ls
-#sudo /usr/local/bin/docker-compose run db2 ls
-sudo /usr/local/bin/docker-compose run web2 bash -c ./restore.sh
-sudo /usr/local/bin/docker-compose run web1 bash -c ./restore.sh
-sudo /usr/local/bin/docker-compose up
+/usr/local/bin/docker-compose build
+/usr/local/bin/docker-compose run web2 bash -c ./restore.sh
+/usr/local/bin/docker-compose run web1 bash -c ./restore.sh
+/usr/local/bin/docker-compose up
--- micro1/docker/clean_all.sh
+++ micro1/docker/clean_all.sh
@@ -1,16 +1,8 @@
-sudo /usr/local/bin/docker-compose stop web1
-sudo /usr/local/bin/docker-compose stop web2
-sudo /usr/local/bin/docker-compose stop db1
-sudo /usr/local/bin/docker-compose stop db2
-sudo /usr/local/bin/docker-compose rm -f web1
-sudo /usr/local/bin/docker-compose rm -f web2
-sudo /usr/local/bin/docker-compose rm -f db1
-sudo /usr/local/bin/docker-compose rm -f db2
-
-#sudo service docker stop
-#sudo rm -rf /var/lib/docker
-#sudo rm -rf log1
-#sudo rm -rf log2
-#sudo rm -rf pgdata1
-#sudo rm -rf pgdata2
-#sudo service docker start
+/usr/local/bin/docker-compose stop web1
+/usr/local/bin/docker-compose stop web2
+/usr/local/bin/docker-compose stop db1
+/usr/local/bin/docker-compose stop db2
+/usr/local/bin/docker-compose rm -f web1
+/usr/local/bin/docker-compose rm -f web2
+/usr/local/bin/docker-compose rm -f db1
+/usr/local/bin/docker-compose rm -f db2
